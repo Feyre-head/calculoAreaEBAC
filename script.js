2fc3e552e27748facbffd91c7361bcc6e67aced2
Feyre <inaraoenning@gmail.com>
2024-10-25 16:16:10 -0400

    Exercicio EBAC

---
+++ script.js
@@ -0,0 +1,12 @@
+function somar(){
+    var base = document.getElementById('base').value
+    var altura = document.getElementById('altura').value
+    var resposta = document.getElementById('res')
+
+    base=Number(base)
+    altura=Number(base)
+    total=base*altura
+
+    res.innerHTML=`Área = ${total}`
+
+}
